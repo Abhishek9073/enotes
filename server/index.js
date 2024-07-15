6002c9a355ca1aa641fb70b6e8cfe22096403310
Abhishek9073 <abhisheksingh9073@gmail.com>
2024-07-15 23:02:42 +0530

    cors updated

--- server/index.js
+++ server/index.js
@@ -9,6 +9,15 @@ const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 5003;
 
+
+// Middleware to set CORS headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://enotes-mern.vercel.app'); // Replace with your frontend URL
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // Middleware
 app.use(express.json());
 app.use(cors(
