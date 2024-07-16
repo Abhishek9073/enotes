248c77e6f608c7f8415ecd3d197bae7e18a619da
Abhishek9073 <142875677+Abhishek9073@users.noreply.github.com>
2024-07-16 07:44:51 +0530

    Update index.js netlify fronend url added

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5003;
 
 // Middleware to set CORS headers
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://enotes-mern.vercel.app'); // Replace with your frontend URL
+  res.setHeader('Access-Control-Allow-Origin', 'https://enotes-mern.netlify.app/'); // Replace with your frontend URL
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(cors(
 {
-  origin:["https://enotes-mern.vercel.app"],
+  origin:["https://enotes-mern.netlify.app/"],
   methods:["GET","POST","PUT","DELETE"],
   credentials:true
 }
